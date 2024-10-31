15c80729b885f4233175d7d1da28f33c46e93e8f
Bobby Iliev <bobby@bobbyiliev.com>
2024-10-31 16:10:31 +0200

    Fix db creation error

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -6,8 +6,8 @@ module "db" {
 
   engine               = "postgres"
   engine_version       = var.postgres_version
-  family               = "postgres13"
-  major_engine_version = "13"
+  family               = "postgres${var.postgres_version}"
+  major_engine_version = var.postgres_version
   instance_class       = var.instance_class
 
   password = var.database_password
@@ -23,14 +23,17 @@ module "db" {
   multi_az               = var.multi_az
   subnet_ids             = var.database_subnet_ids
   vpc_security_group_ids = [aws_security_group.database.id]
+  create_db_subnet_group = true
+  db_subnet_group_name   = "${var.db_identifier}-subnet-group"
 
-  maintenance_window = var.maintenance_window
-  backup_window      = var.backup_window
-
+  maintenance_window      = var.maintenance_window
+  backup_window           = var.backup_window
   backup_retention_period = var.backup_retention_period
   skip_final_snapshot     = true
 
   tags = var.tags
+
+  depends_on = [aws_security_group.database]
 }
 
 resource "aws_security_group" "database" {
@@ -44,5 +47,18 @@ resource "aws_security_group" "database" {
     security_groups = [var.eks_security_group_id]
   }
 
-  tags = var.tags
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.db_identifier}-sg"
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
