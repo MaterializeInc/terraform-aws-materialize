a28daaf0089661fd61a31c7f48f4646bffefb1a6
Bobby Iliev <bobby@bobbyiliev.com>
2024-11-14 19:39:43 +0200

    Disable ebs addon

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,12 +5,6 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  cluster_addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
-  }
-
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnet_ids
 
@@ -38,19 +32,4 @@ module "eks" {
   }
 
   tags = var.tags
-}
-
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
