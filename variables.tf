60b13b325ee5e333ad5a58be1d7adeb576524243
Bobby Iliev <bobby@bobbyiliev.com>
2025-02-09 15:37:58 +0200

    Add rollout options to Helm module

--- variables.tf
+++ variables.tf
@@ -301,8 +301,29 @@ variable "materialize_instances" {
     memory_request       = optional(string, "1Gi")
     memory_limit         = optional(string, "1Gi")
     create_database      = optional(bool, true)
+    in_place_rollout     = optional(bool, false)
+    request_rollout      = optional(string)
+    force_rollout        = optional(string)
   }))
   default = []
+
+  validation {
+    condition = alltrue([
+      for instance in var.materialize_instances :
+      instance.request_rollout == null ||
+      can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", instance.request_rollout))
+    ])
+    error_message = "Request rollout must be a valid UUID in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  }
+
+  validation {
+    condition = alltrue([
+      for instance in var.materialize_instances :
+      instance.force_rollout == null ||
+      can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", instance.force_rollout))
+    ])
+    error_message = "Force rollout must be a valid UUID in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  }
 }
 
 variable "install_metrics_server" {
