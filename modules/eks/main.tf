4c5145205ebdf75a5280e55681f4fe727fe9c4db
Alex Hunt <alex.hunt@materialize.com>
2025-03-06 18:20:00 +0000

    NLB ingress

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -39,6 +39,36 @@ module "eks" {
     }
   }
 
+  node_security_group_additional_rules = {
+    mz_ingress_http = {
+      description      = "Ingress to materialize balancers HTTP"
+      protocol         = "tcp"
+      from_port        = 6876
+      to_port          = 6876
+      type             = "ingress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+    mz_ingress_pgwire = {
+      description      = "Ingress to materialize balancers pgwire"
+      protocol         = "tcp"
+      from_port        = 6875
+      to_port          = 6875
+      type             = "ingress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+    mz_ingress_nlb_health_checks = {
+      description      = "Ingress to materialize balancer health checks and console"
+      protocol         = "tcp"
+      from_port        = 8080
+      to_port          = 8080
+      type             = "ingress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
   # Cluster access entry
   # To add the current caller identity as an administrator
   enable_cluster_creator_admin_permissions = var.enable_cluster_creator_admin_permissions
