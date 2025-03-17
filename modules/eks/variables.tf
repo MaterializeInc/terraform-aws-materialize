6166a168ff61eec7026ab848411fce10285b977b
Bobby Iliev <bobby@bobbyiliev.com>
2025-03-17 13:43:14 +0200

    Add lgalloc support

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -72,3 +72,22 @@ variable "enable_cluster_creator_admin_permissions" {
   type        = bool
   default     = true
 }
+
+# OpenEBS configuration
+variable "install_openebs" {
+  description = "Whether to install OpenEBS for NVMe storage"
+  type        = bool
+  default     = true
+}
+
+variable "openebs_namespace" {
+  description = "Namespace for OpenEBS components"
+  type        = string
+  default     = "openebs"
+}
+
+variable "openebs_version" {
+  description = "Version of OpenEBS Helm chart to install"
+  type        = string
+  default     = "4.2.0"
+}
